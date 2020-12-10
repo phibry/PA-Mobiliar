69bbf006e810b22de46b44d562106ccb0f6b0a60
HIT-91-MAN <buehler1991@gmail.com>
2020-12-10 09:32:52 +0100

    -

---
+++ add/load_files.R
@@ -0,0 +1,11 @@
+source("index_1_onebest_year.RData")
+source("index_2_onebest_year.RData")
+source("index_3_onebest_year.RData")
+source("index_4_onebest_year.RData")
+
+source("index_1_tenbest_year.RData")
+source("index_2_tenbest_year.RData")
+source("index_3_tenbest_year.RData")
+source("index_4_tenbest_year.RData")
+
+source("index_1_onebest_yearwith15.RData")
