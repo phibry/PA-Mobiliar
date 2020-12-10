0b00d24afd279dea5c5eef035342806b1e8db2e9
HIT-91-MAN <buehler1991@gmail.com>
2020-12-10 11:22:30 +0100

    -

--- add/load_files.R
+++ add/load_files.R
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
 
 
 #loading the r files for the sections
 
-=======
->>>>>>> main
-
 
 load("data/R_Files/index_1_tenbest_year.RData")
 load("data/R_Files/index_2_tenbest_year.RData")
-#load("data/R_Files/index_3_tenbest_year.RData")
-#load("data/R_Files/index_4_tenbest_year.RData")
+load("data/R_Files/index_3_tenbest_year.RData")
+load("data/R_Files/index_4_tenbest_year.RData")
 
 
 load("data/R_Files/index_1_onebest_yearwith15.RData")
@@ -23,7 +19,3 @@ load("data/R_Files/index_4_onebest_year.RData")
 
 load("data/R_Files/optim_ma_cross_obj_1.RData")
 load("data/R_Files/optim_ma_cross_obj_1_xts.RData")
-<<<<<<< HEAD
-=======
-
->>>>>>> main
