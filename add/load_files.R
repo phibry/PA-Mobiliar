537441c1469d572d6bbe91a6fc88977bd95543fc
HIT-91-MAN <buehler1991@gmail.com>
2020-12-10 09:12:46 +0100

    -

---
+++ add/load_files.R
@@ -0,0 +1,18 @@
+
+#loading the r files for the sections
+
+
+
+load("data/R_Files/index_1_tenbest_year.RData")
+load("data/R_Files/index_2_tenbest_year.RData")
+#load("data/R_Files/index_3_tenbest_year.RData")
+#load("data/R_Files/index_4_tenbest_year.RData")
+
+load("data/R_Files/index_1_onebest_year.RData")
+load("data/R_Files/index_2_onebest_year.RData")
+load("data/R_Files/index_3_onebest_year.RData")
+load("data/R_Files/index_4_onebest_year.RData")
+
+
+load("data/R_Files/optim_ma_cross_obj_1.RData")
+load("data/R_Files/optim_ma_cross_obj_1_xts.RData")
